1e2517b8e3ca7d93afca3e46ff6054ada2335303
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2025-03-14 23:40:34 -0400

    changes made to app.js

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from 'react';
+import { StyleSheet, SafeAreaView, FlatList, Text } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default function App() {
+
+  let data = [
+    {
+      key: "1",
+      description: "Title",
+      completed: false,
+    },
+    {
+      key: "2",
+      description: "Title2",
+      completed: false,
+    }
+  ]
+
+  let renderItem = ({ item }) => {
+    return <Text>{item.description}</Text>
+  }
+  return (<SafeAreaView style={styles.container}>
+    <FlatList data={data} renderItem={renderItem}></FlatList>
+  </SafeAreaView>)
+}
